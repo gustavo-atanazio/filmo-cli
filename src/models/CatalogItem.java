e2137df995cfcc3c5799fe304c3e182020e1ea6e
Gustavo Atanazio <gus.atanazio@gmail.com>
2025-06-29 21:37:29 -0300

    feat: entidade CatalogItem

---
+++ src/models/CatalogItem.java
@@ -0,0 +1,21 @@
+package models;
+
+public class CatalogItem {
+  private String name;
+  private short releaseYear;
+  private String synopsis;
+
+  public CatalogItem(String name, short releaseYear) {
+    this.name = name;
+    this.releaseYear = releaseYear;
+  }
+
+  public String getName() { return name; }
+  public void setName(String name) { this.name = name; }
+
+  public short getReleaseYear() { return releaseYear; }
+  public void setReleaseYear(short releaseYear) { this.releaseYear = releaseYear; }
+
+  public String getSynopsis() { return synopsis; }
+  public void setSynopsis(String synopsis) { this.synopsis = synopsis; }
+}
